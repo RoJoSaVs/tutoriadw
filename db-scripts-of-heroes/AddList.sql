2e484a9dcc63f025a4141820af98f52822316efc
RoJoSaVs <ronnyjosv259@outlook.es>
2022-07-24 18:40:05 -0600

    Added population scripts

---
+++ db-scripts-of-heroes/AddList.sql
@@ -0,0 +1,12 @@
+INSERT INTO Lista (nombre, listaUsuario) VALUES
+	('main', 'jzjwCR4PLJtvjUAMkVgekSPXs8RbJm'),
+	('main', 'tfdLb4pjRmKNmvgfmUTEgTgs5bb3y2'),
+	('main', 'c7twbyG63wDJCY87AwVS39JZ6qcB2c'),
+	('main', 'Uvudb5kY6hyAXKf6QVuuBvtZ2DguDX'),
+	('main', 'fGTRBR4sdLvqcYLsVaDxX7L9bB8kCw'),
+	('main', 'xSQ9PtkDzpeuusmfWGD59kmjHM4vH8'),
+	('main', 'CZWTV2wuPhaJqWmH8U7xxN2DsRfFUM'),
+	('main', 'kwrymtp8m8BqZDMmNZaNHWwXVTJNsn'),
+	('main', 'uHJMx88amF5dBTPj7jQCdJbQUJmzeE'),
+	('main', 'SAWQvqstkdDgEVLHLDVfXvcgs65apR'),
+	('main', '3GVx6bUV5RuHfmHUZG74nbk7cjUV33');
---
+++ db-scripts-of-heroes/AddTask.sql
@@ -0,0 +1,5 @@
+INSERT INTO Tarea (nombre, descripcion, urgenciaString, urgenciaNumber, fechaVencimiento, 
+					estado, posicion, datosContacto, categoriaLista) VALUES
+	('main', 'descripcion', 'Urgente', 5, '2022-07-22', 'Pendiente', 0, 'datos de contacto', 11),
+	('main2', 'descripcion2', 'Urgente', 1, '2022-07-21', 'Terminada', 1, 'datos de contacto', 11),
+	('main3', 'descripcion3', 'Urgente', 3, '2022-07-20', 'En Proceso', 2, 'datos de contacto', 11);
